87e860e69bb98cd4597c2ce0a65a68f416fbfc30
동동이 <drdragon777@naver.com>
2021-08-07 14:57:12 +0900

    [PG]실패율 - 2019 카카오 채용

---
+++ 김동용_kingyong9169/[PG]실패율.js
@@ -0,0 +1,40 @@
+function solution(N, stages) {
+    let answer = [];
+    for(let i = 1; i<=N; i++){
+        const reach_num = stages.reduce((acc, cur)=>{
+            if(cur >= i)
+                acc.push(cur);
+            return acc;
+        },[]);
+        const fail_num = stages.reduce((acc, cur)=>{
+            if(cur === i)
+                acc.push(cur);
+            return acc;
+        },[]);
+        answer.push(fail_num.length/reach_num.length);
+    }
+
+    let mapped = answer.map((value, i) => ({index: i, prob: value })).sort((a, b) => (b.prob - a.prob));
+    return mapped.reduce((acc, cur)=>(acc.concat(cur.index + 1)),[]);
+}
+
+function solution2(N, stages) { //다른 사람 풀이
+    let ans = []
+
+    for (let i = 1; i <= N; ++i) {
+        let usersReachedCurrentStage   = stages.reduce((acc, curStage) => acc + ((curStage >= i) ? 1 : 0), 0)
+        let usersStagnatedCurrentStage = stages.reduce((acc, curStage) => acc + ((curStage == i) ? 1 : 0), 0)
+        if (usersReachedCurrentStage === 0) {
+            ans.push({ 'stage': i, 'failRate': 0 })
+            continue
+        }
+
+        ans.push({ 'stage': i, 'failRate': (usersStagnatedCurrentStage / usersReachedCurrentStage) })
+    }
+
+    return ans.sort((a, b) => {
+        if (a.failRate > b.failRate) return -1
+        if (a.failRate < b.failRate) return 1
+        return a.stage - b.stage
+    }).map(entry => entry.stage)
+}
