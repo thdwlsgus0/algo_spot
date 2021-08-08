9f9f1f355bc6e9a6016a5d3d4fd8c319d7ed6609
JuYeong <happyu9986@gmail.com>
2021-08-08 21:10:44 +0900

    [PG]소수찾기-못풀었음

---
+++ juyeong/[완전탐색]소수찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    let answer = [...numbers]
+    search(answer)
+    return answer;
+}
+
+const search = (answer)=>{
+    let num = []
+    for(let i=0; i<answer.length; i++){
+    }
+
+    return 
+}
