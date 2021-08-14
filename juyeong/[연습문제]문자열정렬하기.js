cd80851421bd9c5b645b4abe6a49e905ce7b47e2
JuYeong <happyu9986@gmail.com>
2021-08-14 23:07:32 +0900

    [PG]문자열 내 마음대로 정렬하기.js - <연습문제>

--- juyeong/[연습문제]문자열정렬하기.js
+++ juyeong/[연습문제]문자열정렬하기.js
@@ -1,5 +1,5 @@
 function solution(strings, n) {
-    strings.sort((a, b)=>{
+    return strings.sort((a, b)=>{
         if(a.charAt(n) < b.charAt(n))
             return -1
         else if(a.charAt(n) > b.charAt(n))
@@ -11,5 +11,4 @@ function solution(strings, n) {
                 return 1
         }
     })
-    return strings;
 }
