020d141f561111b5933a775184587df11d3a8456
JuYeong <happyu9986@gmail.com>
2021-08-18 20:38:40 +0900

    [PG] 프린터.js - <스택/큐>

--- juyeong/[스택&큐]프린터.js
+++ juyeong/[스택&큐]프린터.js
@@ -1,15 +1,14 @@
 function solution(priorities, location) {
     let count = 0;
     
-    while(priorities.length > 0){
-        
+    while(priorities.length > 0){      
         if(priorities[0] != Math.max(...priorities))    // 첫번째 값이 배열의 최대값이 아닐 경우
             priorities.push(priorities.shift());        // 앞의 거를 뒤로 옮김
         else{           // 프린트 하는 경우
             count++;    // 몇 번째로 프린트 되는지
             if(location === 0)
                 return count;
-            priorities.shift()
+            priorities.shift();
         }
         
         if(location != 0)
