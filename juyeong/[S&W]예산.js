48bcb966cc216c43c5afdf837f120854387945b9
JuYeong <happyu9986@gmail.com>
2021-08-29 03:06:31 +0900

    [PG]예산.js - <S/W>

--- juyeong/[S&W]예산.js
+++ juyeong/[S&W]예산.js
@@ -2,16 +2,13 @@ function solution(d, budget) {
   let sum = 0;
   let count = 0;
   d.sort((a, b) => a - b);
-  console.log(d);
-  for (let i = 0; sum < budget; i++) {
-    sum += d[i];
+  for (let value of d) {
+    sum += value;
     count++;
     if (sum > budget) {
-      sum -= d[i];
       count--;
       break;
     }
   }
-  console.log(sum);
   return count;
 }
