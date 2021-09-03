d5048e6724c54f85df4b83ce87aae3bd236b7c09
동동이 <drdragon777@naver.com>
2021-09-04 05:00:49 +0900

    [PG]문자열 압축 - 2020 카카오 Lv.2

---
+++ 김동용_kingyong9169/[PG]문자열 압축.js
@@ -0,0 +1,27 @@
+function solution(s) { // 테스트 3번 시간 초과, 9번 실패
+    let answer = s.length;
+    if(s.length === 1) return 1;
+    for(let i = 1; i <= s.length / 2; i++){
+        let substr = s.substring(0, i);
+        let nextstr = "";
+        let init = s;
+        let count = 0;
+        for(let j = i; j <= init.length; j += i){
+            if(substr === init.substring(j, j + i)){ // 반복 O
+                count += 1;
+            }
+            else{ // 반복 X
+                nextstr = init.substring(j, j + i);
+                if(count !== 0){
+                    const change = count + 1 + substr;
+                    init = init.replace(substr.repeat(count + 1), change);
+                    j = init.indexOf(change) + change.length;
+                    count = 0;
+                }
+                substr = nextstr;
+            }
+        }
+        answer = Math.min(answer, init.length);
+    }
+    return answer;
+}
