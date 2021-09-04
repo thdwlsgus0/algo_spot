7ef86f7a58fb5e69f8818c37b8e46fafceb5fdfd
JuYeong <happyu9986@gmail.com>
2021-09-04 21:40:57 +0900

    [PG]문자열 압축 - for문 반복횟수 수정

--- juyeong/[2020카카오]문자열압축.js
+++ juyeong/[2020카카오]문자열압축.js
@@ -3,16 +3,17 @@ function solution(s) {
   let min = slen;
   if (slen === 1) return 1;
 
-  for (let i = 1; i <= slen; i++) {
+  for (let i = 1; i <= slen / 2; i++) {
     let count = 1;
     let result = 0;
     for (let j = 0; j < slen; j += i) {
       const sub1 = s.substr(j, i);
       const sub2 = s.substr(j + i, i);
-      if (sub1 === sub2) count++;
-      else {
+      if (sub1 === sub2) {
+        count++;
+      } else {
         count > 1
-          ? count >= 10 // count가 10넘어가는 경우는 숫자 2자리를 더해줘야함
+          ? count >= 10
             ? (result += sub1.length + 2)
             : (result += sub1.length + 1)
           : (result += sub1.length);
