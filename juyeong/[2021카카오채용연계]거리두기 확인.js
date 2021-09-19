73f9d5fced4d3e95cb79c5827b6c0feb367fdcf7
JuYeong <happyu9986@gmail.com>
2021-09-19 20:12:13 +0900

    [PG]-거리두기확인하기.js-<2021카카오>

--- juyeong/[2021카카오채용연계]거리두기 확인.js
+++ juyeong/[2021카카오채용연계]거리두기 확인.js
@@ -1,52 +1,55 @@
-function solution(places) {
-  const result = [];
+// P가 있는지
+const isP = (place) => place === "P";
 
-  places.forEach((room) => {
-    room.push(["X", "X", "X", "X", "X"]);
-    const rlen = result.length;
-    for (let m = 0; m < 5; m++) {
-      const row = room[m];
-      for (let n = 0; n < 4; n++) {
-        const curr = row[n];
-        const left = row[n - 1];
-        const right = row[n + 1];
-        const down = room[m + 1][n];
-        const left_diag = room[m + 1][n - 1];
-        const right_diag = room[m + 1][n + 1];
-        console.log(row, curr, right, down);
-        if (curr === "P") {
-          // P가 있으면
-          if ([right, down].includes("P")) {
-            // 오른쪽에 P가 있을 경우
-            result.push(0);
-            console.log("오른", result);
-            break;
+// O가 있는지
+const isO = (place) => place === "O";
+
+const isRange = (r) => r >= 0 && r < 5;
+
+const exploreRoom = (place) => {
+  for (let i = 0; i < place.length; i++) {
+    for (let j = 0; j < place[i].length; j++) {
+      console.log(place, place[i], place[i][j]);
+      if (place[i][j] === "P") {
+        if (isRange(i + 1)) {
+          // 1,2,3 => 4 or 5번째 줄이 아닐 경우
+          if (isP(place[i + 1][j])) return false;
+          if (isO(place[i + 1][j])) {
+            if (isRange(i + 2) && isP(place[i + 2][j])) return false;
+            if (isRange(j - 1) && isP(place[i + 1][j - 1])) return false;
+            if (isRange(j + 2) && isP(place[i + 1][j + 1])) return false;
           }
-          if ([right_diag].includes("P") && [right, down].includes("O")) {
-            // 오른쪽 대각선에 P가 있을 경우
-            result.push(0);
-            console.log("오른대각", result);
-            break;
+        }
+        if (isRange(i - 1)) {
+          // 2,3,4,5
+          if (isP(place[i - 1][j])) return false;
+          if (isO(place[i - 1][j])) {
+            if (isRange(i - 2) && isP(place[i - 2][j])) return false;
+            if (isRange(j - 1) && isP(place[i - 1][j - 1])) return false;
+            if (isRange(j + 1) && isP(place[i - 1][j + 1])) return false;
           }
-          if ([left_diag].includes("P") && [left, down].includes("O")) {
-            // 왼쪽 대각선에 P가 있을 경우
-            result.push(0);
-            console.log("왼대각", result);
-            break;
-          }
+        }
+        if (isRange(j + 1)) {
+          //
+          if (isP(place[i][j + 1])) return false;
+          if (isO(place[i][j + 1]) && isRange(j + 2) && isP(place[i][j + 2]))
+            return false;
+        }
+        if (isRange(j - 1)) {
+          if (isP(place[i][j - 1])) return false;
+          if (isO(place[i][j - 1]) && isRange(j - 2) && isP(place[i][j - 2]))
+            return false;
         }
       }
     }
-    if (rlen !== result.length)
-      // 다음 방으로 옮기기
-      return;
-    // 모든 조건 만족
-    else result.push(1);
-    console.log(result);
-  });
+  }
+  return true;
+};
+
+function solution(places) {
+  const result = [];
+  for (const place of places) {
+    exploreRoom(place) ? result.push(1) : result.push(0);
+  }
   return result;
 }
-// P가 있으면, -> i
-// 1. 하우에 P가 있을 경우 0push -> j+1, i+1
-// 2. 대각선에 P가 있을 경우, 내 아래나 대각선 P의 위에 O가 있을 경우 0push
-// 3. 그렇지 않을 경우 그대로 반복하고 0으로 break되지 않았다면 마지막에 1push
