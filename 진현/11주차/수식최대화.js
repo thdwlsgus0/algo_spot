475ccf9833833b3d614a610d6b5602ded99fa22b
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-10-30 23:23:55 +0900

    수식 최대화 코드 수정

--- 진현/11주차/수식최대화.js
+++ 진현/11주차/수식최대화.js
@@ -1,20 +1,26 @@
-function solution(expression) {
+const operator = [
+    ['*', '+', '-'],
+    ['*', '-', '+'],
+    ['+', '*', '-'],
+    ['+', '-', '*'],
+    ['-', '*', '+'],
+    ['-', '+', '*']
+];
+
+const selectOperator = (copyOperator, copyNum, index) => {
+    switch(copyOperator[index]) {
+        case "+":
+            return copyNum[index] + copyNum[index+1];
+        case "-":
+            return copyNum[index] - copyNum[index+1];
+        case "*":
+            return copyNum[index] * copyNum[index+1];    
+    }  
+}
+
+const getAnswer = (expression, operators, num) => {
     let answer = 0;
-    const operator = [
-        ['*', '+', '-'],
-        ['*', '-', '+'],
-        ['+', '*', '-'],
-        ['+', '-', '*'],
-        ['-', '*', '+'],
-        ['-', '+', '*']
-    ];
-    let num = expression.split(/[^0-9]/);
-    let operators = expression.split(/\w{1,}/);
-    operators.shift();
-    operators.pop();
-    
-    num = num.map(v => +v);
-    
+
     for(let i = 0; i < operator.length; i++) {
         let copyNum = num.slice();
         let copyOperator = operators.slice();
@@ -22,24 +28,28 @@ function solution(expression) {
         for(let j = 0; j < operator[i].length; j++) {
             for(let k = 0; k < copyOperator.length; k++) {
                 if(operator[i][j] === copyOperator[k]) {
-                    if(copyOperator[k] === '*') {
-                        copyNum[k] *= copyNum[k+1];
-                    }else if(copyOperator[k] === '+'){
-                        copyNum[k] += copyNum[k+1];
-                    }else if(copyOperator[k] === '-') {
-                        copyNum[k] -= copyNum[k+1];
-                    }
+                    copyNum[k] = selectOperator(copyOperator, copyNum, k);
                     copyNum.splice(k+1, 1);
                     copyOperator.splice(k, 1);
                     k--;
                 }
             }
-         
         }
         answer = Math.max(Math.abs(answer), Math.abs(copyNum[0]));
     }
     
     return answer;
+}
+
+function solution(expression) {
+
+    let num = expression.split(/[^0-9]/);
+    let operators = expression.split(/\w{1,}/);
+    operators.shift();
+    operators.pop();
+    
+    num = num.map(v => +v);
+    return getAnswer(expression, operators, num);
 }
 
 console.log(solution("100-200*300-500+20"));
