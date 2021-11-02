ed38de9b02c61cae3e0b61cef21e76de7660a176
동동이 <drdragon777@naver.com>
2021-11-02 23:55:40 +0900

    [PG]수식 최대화 - 카카오 피드백 반영

--- 김동용_kingyong9169/[PG]수식 최대화.js
+++ 김동용_kingyong9169/[PG]수식 최대화.js
@@ -9,14 +9,14 @@ function solution(expression) {
     ];
     const answer = [];
     for(const opSet of prior){
-        const set = expression.split(/(\D)/g);
+        const set = expression.split(/(\D)/g).map(v => +v);
         for(const op of opSet){
             while(set.includes(op)){
                 const index = set.indexOf(op);
                 let calc = 0;
                 if(op === '+') calc = +set[index - 1] + +set[index + 1];
-                else if(op === '-') calc = +set[index - 1] - +set[index + 1];
-                else if(op === '*') calc = +set[index - 1] * +set[index + 1];
+                else if(op === '-') calc = set[index - 1] - set[index + 1];
+                else if(op === '*') calc = set[index - 1] * set[index + 1];
                 set.splice(index - 1, 3, calc);
             }
         }
