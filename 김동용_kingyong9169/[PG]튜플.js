873d5bbfcf7017fcea38c4837af34732790a532b
동동이 <drdragon777@naver.com>
2021-11-07 22:48:42 +0900

    solve: [PG] 튜플 리팩토링 - level2

--- 김동용_kingyong9169/[PG]튜플.js
+++ 김동용_kingyong9169/[PG]튜플.js
@@ -1,9 +1,10 @@
 function solution(s) {
+  // 더 빠름 : 배열에 숫자가 있는지 확인
   const answer = [];
   s = s
     .slice(2, s.length - 2)
     .split("},{")
-    .map((x) => x.split(",").reduce((acc, cur) => [...acc, +cur], []))
+    .map((x) => x.split(",").map((v) => +v))
     .sort((a, b) => a.length - b.length);
   for (let i = 0; i < s.length; i++) {
     for (let j = 0; j < s[i].length; j++) {
@@ -11,4 +12,20 @@ function solution(s) {
     }
   }
   return answer;
+}
+
+function solution(s) {
+  // 더 느림 : 배열에 문자가 있는지 확인
+  const answer = [];
+  s = s
+    .slice(2, s.length - 2)
+    .split("},{")
+    .map((x) => x.split(","))
+    .sort((a, b) => a.length - b.length);
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s[i].length; j++) {
+      if (!answer.includes(s[i][j])) answer.push(s[i][j]);
+    }
+  }
+  return answer.map((x) => +x);
 }
