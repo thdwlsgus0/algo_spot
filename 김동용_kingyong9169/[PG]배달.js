9eb73790f61d8acfb2a292325b22f10dda8b4a3c
동동이 <drdragon777@naver.com>
2021-12-22 04:41:58 +0900

    solve: [PG] 배달 - level2, 선형탐색

--- 김동용_kingyong9169/[PG]배달.js
+++ 김동용_kingyong9169/[PG]배달.js
@@ -1,4 +1,4 @@
-function solution(N, road, K) {
+function solution(N, road, K) { // 우선순위 큐
     const dist = new Array(N + 1).fill(Infinity);
     const adj = Array.from({length: N + 1}, () => []);
     const pq = [];
@@ -29,4 +29,49 @@ function solution(N, road, K) {
     }
 
     return dist.filter((time) => time <= K).length;
+}
+
+function solution(N, road, K){ // 선형탐색
+    const dist = new Array(N);
+    const graph = Array.from({length: N}, () => Array(N).fill(Infinity));
+    const isVisited = new Array(N).fill(false);
+    
+    road.forEach(([start, target, time]) => {
+        if(graph[start - 1][target - 1] !== Infinity && graph[start - 1][target - 1] < time) return;
+        graph[start - 1][target - 1] = time;
+        graph[target - 1][start - 1] = time;
+    });
+    
+    dijkstra(N, dist, isVisited, graph);
+    return dist.filter((time) => time <= K).length;
+}
+
+function getSmallIndex(N, dist, isVisited){
+    let minDist = Infinity;
+    let minIdx = -1;
+    for(let i = 0 ; i < N ; i++){
+        if(dist[i] < minDist && !isVisited[i]){
+            minDist = dist[i];
+            minIdx = i;
+        }
+    }
+    return minIdx;
+}
+
+function dijkstra(N, dist, isVisited, graph){
+    for(let i = 0 ; i < N ; i++){
+        dist[i] = graph[0][i];
+    }
+    
+    dist[0] = 0;
+    isVisited[0] = true;
+    
+    for(let i = 0 ; i < N - 1 ; i++){
+        const current = getSmallIndex(N, dist, isVisited);
+        isVisited[current] = true;
+        for(let j = 0 ; j < N ; j++){
+            if(!isVisited[j] && (dist[j] > dist[current] + graph[current][j]))
+                dist[j] = dist[current] + graph[current][j];
+        }
+    }
 }
