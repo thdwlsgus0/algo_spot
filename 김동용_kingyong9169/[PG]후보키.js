163cfc10f70c0c5e97d035b01c3538f8029f4bfc
동동이 <drdragon777@naver.com>
2022-01-06 19:27:36 +0900

    solve: [PG] 후보키 - level2

--- 김동용_kingyong9169/[PG]후보키.js
+++ 김동용_kingyong9169/[PG]후보키.js
@@ -1,56 +1,51 @@
-function getDegree(num) {
-  const result = [];
-  for (let i = 0; i < num; i++) {
-    result.push(i);
-  }
-  return result;
-}
-
 function solution(relation) {
   const answer = [];
   const result = [];
-  const degree = getDegree(relation[0].length);
-  for (let i = 0; i < relation[0].length; i++) {
-    result.push(...getCombination(degree, i + 1));
+  const degree = Array.from({ length: relation[0].length }, (v, i) => i);
+  
+  for(let i = 0 ; i < relation[0].length ; i++){
+      result.push(...getCombination(degree, i + 1));
   }
-  for (let i = 0; i < result.length; i++) {
-    const curList = [];
-    for (let j = 0; j < relation.length; j++) {
-      let candidate = [];
-      let cur = "",
-        valid = true;
-      for (let k = 0; k < result[i].length; k++) {
-        cur += relation[j][result[i][k]];
-        candidate += result[i][k];
+  
+  for(let i = 0 ; i < result.length ; i++){
+      const curList = [];
+      const candidate = result[i];
+      let valid = true;
+      
+      for(let j = 0 ; j < answer.length ; j++){ // 최소성 검사
+          if(answer[j].every((v) => candidate.includes(v))){
+              valid = false;
+              break;
+          }
       }
-
-      if (curList.includes(cur)) break;
-      else curList.push(cur);
-
-      for (let l = 0; l < answer.length; l++) {
-        if (candidate.includes(answer[l])) {
-          valid = false;
-          break;
-        }
+      
+      for(let j = 0 ; valid, j < relation.length ; j++){ // 유일성 검사
+          let cur = "";
+          for(let k = 0 ; k < result[i].length ; k++)
+              cur += relation[j][result[i][k]];
+          
+          if(curList.includes(cur)) {
+              valid = false;
+              break;
+          }
+          curList.push(cur);
       }
-
-      if (valid && j === relation.length - 1) answer.push(candidate);
-    }
+      
+      if(valid) answer.push(candidate);
   }
-  console.log(answer);
   return answer.length;
 }
 
-function getCombination(arr, num) {
+function getCombination(arr, num){
   const result = [];
-  if (num === 1) return arr.map((v) => [v]);
-
+  if(num === 1) return arr.map((v) => [v]);
+  
   arr.forEach((cur, idx, origin) => {
-    const rest = origin.slice(idx + 1);
-    const combinations = getCombination(rest, num - 1);
-    const attached = combinations.map((combination) => [cur, ...combination]);
-    result.push(...attached);
+      const rest = origin.slice(idx + 1);
+      const combinations = getCombination(rest, num - 1);
+      const attached = combinations.map((combination) => [cur, ...combination]);
+      result.push(...attached);
   });
-
+  
   return result;
 }
