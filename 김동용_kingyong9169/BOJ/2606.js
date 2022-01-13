37c515cf638cf05fa24febbe3a42bfd904c21934
동동이 <drdragon777@naver.com>
2022-01-13 23:32:24 +0900

    solve: [backjoon] 2606 바이러스 - silver3

---
+++ 김동용_kingyong9169/BOJ/2606.js
@@ -0,0 +1,55 @@
+function solution(num, arr){ // 처음 내 풀이
+    const graph = Array.from({ length: num[0] + 1 }, () => []);
+    const visited = Array.from({ length: num[0] + 1 }).fill(false);
+    for(let i = 0 ; i < arr.length ; i++){
+        const [from, to] = arr[i];
+        graph[from].push(to);
+        graph[to].push(from);
+    }
+
+    visited[1] = true;
+    dfs(graph[1], graph, visited);
+    console.log(visited.filter((x) => x ).length - 1);
+}
+
+function dfs(edges, graph, visited){
+    edges.forEach((edge) => {
+        if(!visited[edge]){
+            visited[edge] = true;
+            dfs(graph[edge], graph, visited);
+        }
+    });
+}
+
+function solution(num, arr){ // 반복문 풀이
+    const graph = Array.from({ length: num[0] + 1 }, () => []);
+    const visited = Array.from({ length: num[0] + 1 }).fill(false);
+    const pq = [];
+    for(let i = 0 ; i < arr.length ; i++){
+        const [from, to] = arr[i];
+        graph[from].push(to);
+        graph[to].push(from);
+    }
+
+    visited[1] = true;
+    pq.push(1);
+    
+    while(pq.length){
+        const vertex = pq.pop();
+        graph[vertex].forEach((edge) => {
+            if(!visited[edge]){
+                visited[edge] = true;
+                pq.push(edge);
+            }
+        });
+    }
+    console.log(visited.filter((x) => x ).length - 1);
+}
+
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const num = [parseInt(input[0]), parseInt(input[1])];
+const arr = [];
+
+for(let i = 2 ; i < input.length ; i++) arr.push(input[i].split(' ').map((v) => parseInt(v)));
+
+solution(num, arr);
